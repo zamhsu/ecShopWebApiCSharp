b09f2a707c3f54c9d3523dc17f82dc049b231d54
zamhsu <sam2009mn32@gmail.com>
2021-11-17 11:00:07 +0800

    移除可設定狀態

--- Dtos/Members/CreateAdminMemberModel.cs
+++ Dtos/Members/CreateAdminMemberModel.cs
@@ -6,6 +6,5 @@ namespace WebApi.Dtos.Members
         public string Email { get; set; }
         public string Account { get; set; }
         public string Pwd { get; set; }
-        public int StatusId { get; set; }
     }
 }
