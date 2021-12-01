004926fea0e98c8d5b805854579260fbc7ee558f
zamhsu <sam2009mn32@gmail.com>
2021-12-01 22:07:11 +0800

    加入UnitOfWork和執行SQL語句介面

---
+++ Base/ISqlCommand.cs
@@ -0,0 +1,77 @@
+namespace WebApi.Base
+{
+    public interface ISqlCommand
+    {
+        /// <summary>
+        /// 查詢
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql">SQL語句</param>
+        /// <param name="param">參數</param>
+        /// <returns></returns>
+        IEnumerable<T> Query<T>(string sql, object? param = null);
+
+        /// <summary>
+        /// 查詢
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql">SQL語句</param>
+        /// <param name="param">參數</param>
+        /// <returns></returns>
+        Task<IEnumerable<T>> QueryAsync<T>(string sql, object? param = null);
+
+        /// <summary>
+        /// 查詢一筆
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql">SQL語句</param>
+        /// <param name="param">參數</param>
+        /// <returns></returns>
+        T QueryFirstOrDefault<T>(string sql, object? param = null);
+
+        /// <summary>
+        /// 查詢一筆
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql">SQL語句</param>
+        /// <param name="param">參數</param>
+        /// <returns></returns>
+        Task<T> QueryFirstOrDefaultAsync<T>(string sql, object? param = null);
+
+        /// <summary>
+        /// 執行非查詢動作
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql">SQL語句</param>
+        /// <param name="param">參數</param>
+        /// <returns></returns>
+        int Execute(string sql, object? param = null);
+
+        /// <summary>
+        /// 執行非查詢動作
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql">SQL語句</param>
+        /// <param name="param">參數</param>
+        /// <returns></returns>
+        Task<int> ExecuteAsync(string sql, object? param = null);
+
+        /// <summary>
+        /// 查詢所傳回之結果集中第一個資料列的第一個資料行
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql">SQL語句</param>
+        /// <param name="param">參數</param>
+        /// <returns></returns>
+        T ExecuteScalar<T>(string sql, object? param = null);
+
+        /// <summary>
+        /// 查詢所傳回之結果集中第一個資料列的第一個資料行
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql">SQL語句</param>
+        /// <param name="param">參數</param>
+        /// <returns></returns>
+        Task<T> ExecuteScalarAsync<T>(string sql, object? param = null);
+    }
+}
---
+++ Base/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+namespace WebApi.Base
+{
+    public interface IUnitOfWork
+    {
+        /// <summary>
+        /// 儲存變更
+        /// </summary>
+        /// <returns></returns>
+        int SaveChanges();
+
+        /// <summary>
+        /// 非同步儲存變更
+        /// </summary>
+        /// <returns></returns>
+        Task<int> SaveChangesAsync();
+    }
+}
